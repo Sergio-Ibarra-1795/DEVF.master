34dbcf06d938a0efe4591bedccb5c804df58db15
Sergio-Ibarra-1795 <sergio.ibarra1795@gmail.com>
2022-08-22 11:28:47 -0500

    Updating folder with Dell desktop at 22.August.2022 111_30 am

---
+++ 03_Computer_science/POO/Estructuras01/Pila03.js
@@ -0,0 +1,56 @@
+/*3.- Un conductor maneja de un pueblo origen a un pueblo destino, pasando por varios
+pueblos. Una vez en el pueblo destino, el conductor debe regresar a casa por el mismo
+camino. Muestre el camino recorrido tanto de ida como de vuelta. */
+
+
+//Para implementar una pila en JS
+class Stack {
+    constructor(){
+        this.item = [] 
+    }
+
+    push(item){ 
+        this.item.push(item);
+    }
+
+    pop(){ 
+       return this.item.pop(); 
+    }
+
+    peak(){ 
+        return this.item(this.item.length-1);
+     }
+
+    size(){ 
+        return this.item.length; 
+     }
+    
+    print(){ 
+        console.log(this.item) 
+     }
+
+
+}
+
+let stack03 = new Stack ()
+
+stack03.push("Partida")
+stack03.push("Castillo embrujado")
+stack03.push("Una puertota")
+stack03.push("El camellom")
+stack03.push("El acuario")
+stack03.push("Meta")
+
+stack03.print();
+
+let camino = function(stack){
+    let new_camino = new Stack();
+    let elemento;
+    for(i=0;i<stack.size()+5;i++){
+       elemento = stack.pop()
+       new_camino.push(elemento)
+    }
+    return new_camino
+}
+
+console.log(camino(stack03));
---
+++ 03_Computer_science/POO/Estructuras01/Pila04.js
@@ -0,0 +1,8 @@
+/*4.-  Un almacén tiene capacidad para apilar “n” contenedores.
+ Cada contenedor tiene una posición. Cuando se desea retirar
+  un contenedor específico, deben retirarse primero los 
+  contenedores que están encima de él y colocarlos en otra
+   pila,efectuar el retiro y regresarlos a su respectivo lugar.*/
+
+
+   
