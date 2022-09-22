357aeb9facc0ad10076816f98ee8f7757a293a55
Sergio-Ibarra-1795 <sergio.ibarra1795@gmail.com>
2022-09-21 21:13:55 -0500

    Ejemplo de implementación de función getElement en un API de pokemon

---
+++ JS_avanzado/API/API.0.js
@@ -0,0 +1,26 @@
+//Vamos a consumir la pokeapi (https://pokeapi.co/)
+//ENDPOINT PARA HACER PETICIONES: https://pokeapi.co/api/v2/pokemon/
+
+
+// 1) Traer a request 
+const request = require('request');
+
+// 2) Crear URI (QUE TOERNE EL ENDPPINT)
+const URI = 'https://pokeapi.co/api/v2/pokemon/'
+
+// 2) Implementar a request (Crear una funcion que piuda el nombre y devuelva tipo)
+function getPokemon (name){
+   request(URI + name, function (error, response, body){
+    if(response.statusCode === 200){
+        const dataEnFormatoJson = JSON.parse(body)//Pasar de un objeto JSON a uno de JS
+        const typePokemon = dataEnFormatoJson.types.map((objeto=>objeto.type.name))
+        console.log(`El tipo de pokemon de ${name} es ${typePokemon}`)
+    } else {
+        console.log(`Ocurrió un error: ${response.statusCode}, ${response.statusMessage}`)
+    }
+
+   });
+
+}
+
+getPokemon('pikachu')
