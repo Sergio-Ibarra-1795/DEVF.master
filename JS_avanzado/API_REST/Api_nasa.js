de649b5785f98de81d1f9d8ded17b204e3905032
Sergio-Ibarra-1795 <sergio.ibarra1795@gmail.com>
2022-09-25 22:19:57 -0500

    Desplegando una foto de una API de la nasa con ayuda del DOM y la consulta de APIs

--- JS_avanzado/API_REST/Api_nasa.js
+++ JS_avanzado/API_REST/Api_nasa.js
@@ -1,5 +1,5 @@
 console.log("API de la NASA")
-import fetch from "node-fetch"
+//import fetch from "node-fetch"
 
 const llave = "qnYqEcs6ZysAb4h3SmFaupoPc92V6UfxS5DgRqLC"
 const APInasa = `https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=${llave}`
@@ -21,4 +21,30 @@ async function apiNasa(url){
     })
 }
 
-apiNasa(APInasa)
+//apiNasa(APInasa)
+
+let im
+let IDim
+//EJEMPLO DE USO DE OTRO ENDPOINT 
+async function fotosMarte(key, robot,camera, dia){
+    let urlMarte = `https://api.nasa.gov/mars-photos/api/v1/rovers/${robot}/photos?sol=${dia}&camera=${camera}&api_key=${key}`
+    const consulta2 = await fetch(urlMarte)
+    let respuestaJson2 = await consulta2.json()
+    console.log(respuestaJson2)
+    let lista_fotos = respuestaJson2.photos
+    lista_fotos.forEach((elemento,indice,arreglo)=>{
+
+        im = elemento.img_src
+        IDim =elemento.id
+    })
+
+
+} 
+
+async function traerfoto(){
+    let contenedor = document.getElementById('contenedorFoto')
+    
+    await fotosMarte(llave,'curiosity','FHAZ','1000')
+    contenedor.innerHTML = `<img src=${im} alt=${IDim} >`
+}
+
