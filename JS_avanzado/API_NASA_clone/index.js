7cc540682921b43cd65ff3ea189ce480ce364a8e
Sergio-Ibarra-1795 <sergio.ibarra1795@gmail.com>
2022-09-26 22:14:00 -0500

    API NASA y paginación

--- JS_avanzado/API_NASA_clone/index.js
+++ JS_avanzado/API_NASA_clone/index.js
@@ -30,12 +30,18 @@ async function apinasa(url){
 
 
 //apinasa(apiNasa)
-
+var contadorpagina =1
+var diasolarv = ""
+var roverv = ""
+var sensorv = ""
 async function fotosMarte(key,robot,camara,dia){
+    var numerop=document.getElementById("paginahtml")
+    numerop.innerHTML = `<a href="#" class="link-success" > ${contadorpagina} </a>`
+    console.log(contadorpagina)
     var contenedor =document.getElementById("contenedorFoto")
     var fotodia = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
     var fotodiajson = await fotodia.json()
-    var urlmarte = `https://api.nasa.gov/mars-photos/api/v1/rovers/${robot}/photos?sol=${dia}&camera=${camara}&api_key=${key}`
+    var urlmarte = `https://api.nasa.gov/mars-photos/api/v1/rovers/${robot}/photos?sol=${dia}&camera=${camara}&page=${contadorpagina}&api_key=${key}`
     const respuestaurl = await fetch(urlmarte)
     var respjson = await respuestaurl.json()
     console.log(respjson)
@@ -44,8 +50,6 @@ async function fotosMarte(key,robot,camara,dia){
     contenedor.innerHTML = ""
     if (listafotos.length==0){
         console.log(fotodiajson)
-        //contenedor.classList.remove("mt-4")
-        //contenedor.classList.add("mt-12")
         contenedor.innerHTML = `
         <div class="card" style="width: 100%;">
         <img class="card-img-top" src=${fotodiajson.url} alt=${fotodiajson.media_type}>
@@ -59,34 +63,60 @@ async function fotosMarte(key,robot,camara,dia){
         `
 
     }
+    else if (listafotos.length == 25){
+        let botonant = document.getElementById("botonanterior")
+        let botonsig = document.getElementById("botonsiguiente")
+        listafotos.forEach((elemento,indice,arreglo)=>{
+            contenedor.innerHTML += `
+            <div class="card mb-2" style="width: 18rem;">
+            <img class="card-img-top" src=${elemento.img_src} alt=${elemento.id}>
+            <div class="card-body">
+              <h5 class="card-title">${elemento.rover.name}</h5>
+              <p class="card-text">${elemento.camera.full_name}</p>
+              
+            </div>
+          </div>      
+            
+            `
+            botonsig.classList.remove("ocultar")
+            if(contadorpagina>1){
+                botonant.classList.remove("ocultar")
+            }
+            if(contadorpagina ==1){
+                console.log("YA ENTRO A ESTA LINEA")
+                botonant.classList.add("ocultar")
+            }
 
-    else if(listafotos.length==25){
-
-        
+    
+        })
 
     }
+
+    else {
+        listafotos.forEach((elemento,indice,arreglo)=>{
+            contenedor.innerHTML += `
+            <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src=${elemento.img_src} alt=${elemento.id}>
+            <div class="card-body">
+              <h5 class="card-title">${elemento.rover.name}</h5>
+              <p class="card-text">${elemento.camera.full_name}</p>
+              
+            </div>
+          </div>`
     
+        })
+    
+    }
 
-    listafotos.forEach((elemento,indice,arreglo)=>{
-        contenedor.innerHTML += `
-        <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src=${elemento.img_src} alt=${elemento.id}>
-        <div class="card-body">
-          <h5 class="card-title">${elemento.rover.name}</h5>
-          <p class="card-text">${elemento.camera.full_name}</p>
-          
-        </div>
-      </div>`
-
-    })
+   
 }
 async function traerfoto(){
     let rover = document.getElementById("rover")
     let sensor = document.getElementById("sensor")
     let diasolar = document.getElementById("diasolar")
-    let roverv = rover.value
-    let sensorv = sensor.value
-    let diasolarv = diasolar.value
+    roverv = rover.value
+    sensorv = sensor.value
+    diasolarv = diasolar.value
     console.log(roverv, sensorv, diasolarv)
     await fotosMarte(llave,roverv,sensorv,String(diasolarv))
     //var contenedor =document.getElementById("contenedorFoto")
@@ -96,4 +126,12 @@ async function traerfoto(){
     //    <img src = ${im} alt= ${idim}>
     //`
 
+}
+async function anterior(){
+    contadorpagina = contadorpagina-1
+    await fotosMarte(llave,roverv,sensorv,String(diasolarv))
+}
+async function siguiente(){
+    contadorpagina++
+    await fotosMarte(llave,roverv,sensorv,String(diasolarv))
 }
