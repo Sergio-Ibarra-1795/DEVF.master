d7d78f7ce2d790af35eba06b0c8f77ebeb785525
Sergio-Ibarra-1795 <sergio.ibarra1795@gmail.com>
2022-09-26 20:32:57 -0500

    ads

--- JS_avanzado/API_REST/Api_nasa.js
+++ JS_avanzado/API_REST/Api_nasa.js
@@ -27,12 +27,41 @@ let im
 let IDim
 //EJEMPLO DE USO DE OTRO ENDPOINT 
 async function fotosMarte(key, robot,camera, dia){
+
+    let contenedor = document.getElementById("contenedorFoto")
+    let fotodia = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=DEMO_KEY`)
+    let fotodiaJson = await fotodia.json()
+
     let urlMarte = `https://api.nasa.gov/mars-photos/api/v1/rovers/${robot}/photos?sol=${dia}&camera=${camera}&api_key=${key}`
     const consulta2 = await fetch(urlMarte)
     let respuestaJson2 = await consulta2.json()
     console.log(respuestaJson2)
+    if(lista_fotos.length==0){
+        contenedor.innerHTML= `
+        <div class="card" style="width: 100%;">
+        <img class="card-img-top" src=${fotodiaJson.url} alt=${fotodiaJson.media_type}>
+        <div class="card-body">
+          <h5 class="card-title">${fotodiaJson.tittle}</h5>
+          <p class="card-text">${fotodiaJson.explanatio}</p>
+        </div>
+      </div>
+
+        `
+    }
+    contenedor.innerHTML =""
+
+
     let lista_fotos = respuestaJson2.photos
     lista_fotos.forEach((elemento,indice,arreglo)=>{
+        contenedor.innerHTML+=`
+        <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src=${elemento.img_src} alt=${elemento.id}>
+  <div class="card-body">
+    <h5 class="card-title">${elemento.rover.name}</h5>
+    <p class="card-text">${elemento.camera.full_name}</p>
+  </div>
+</div>
+        `
 
         im = elemento.img_src
         IDim =elemento.id
@@ -42,9 +71,21 @@ async function fotosMarte(key, robot,camera, dia){
 } 
 
 async function traerfoto(){
+    let rover = document.getElementById("rover")
+    let sensor = document.getElementById("sensor")
+    let diasolar  = document.getElementById("diasolar")
     let contenedor = document.getElementById('contenedorFoto')
     
-    await fotosMarte(llave,'curiosity','FHAZ','1000')
-    contenedor.innerHTML = `<img src=${im} alt=${IDim} >`
+    let rovervalue= rover.value
+    let sensorvalue= sensor.value
+    let diasolarvalue= diasolar.value
+
+    console.log(rovervalue,sensorvalue,diasolarvalue)
+
+    await fotosMarte(llave,rovervalue,sensorvalue,String(diasolarvalue))
+    
+
+    //await fotosMarte(llave,'curiosity','FHAZ','1000')
+    //contenedor.innerHTML = `<img src=${im} alt=${IDim} >`
 }
 
