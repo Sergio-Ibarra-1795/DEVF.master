25f7853c17c13b7fedbfde4641cb921d1cfd0c5c
Sergio-Ibarra-1795 <sergio.ibarra1795@gmail.com>
2022-09-29 20:18:00 -0500

    GET & POST a new author

--- JS_avanzado/CRUD/API_crud.js
+++ JS_avanzado/CRUD/API_crud.js
@@ -1,1 +1,27 @@
-//Vamos a ejecutar la lógica 
+//Aqui vamos a ejecutar la lógica 
+const goodReadsCrud = require ('./logica')
+
+//* LISTAR A TODOS LOS AUTORES 
+//goodReadsCrud.listAuthors()
+
+
+//* VER A CADA AUTHOR POR SEPARADO
+//goodReadsCrud.getAuthor(14892)
+
+
+
+//* CREAR UN AUTOR
+
+const jsonsend = {
+
+    name: "Chikito22",
+    last_name: "Peralta",
+    nacionalidad: "MX",
+    biography: "Corazao",
+    gender: "F",
+    age: 28,
+    is_alive: true
+}
+
+
+goodReadsCrud.createAuthor(jsonsend)
--- JS_avanzado/CRUD/logica.js
+++ JS_avanzado/CRUD/logica.js
@@ -1,1 +1,66 @@
 //Vamos a tener nuestra lógica (peticiones)
+//*DOCUMENTACIÓN
+//*URI DE LA API: https://goodreads-devf-aaron.herokuapp.com/api/v1/authors/
+
+//1)Treaer a request que acabamos de instalar 
+const request = require('request')
+
+//? 2) TENER NUESTRO ENDPOINT 
+const URI = 'https://goodreads-devf-aaron.herokuapp.com/api/v1/authors/'
+
+//3 LISTAR A TODOS LOS AUTORES (authors)
+const listAuthors = ( ) => {
+    request.get(URI,(error,response,body)=>{
+        //validamos nuestra petición
+        if(response.statusCode === 200){
+            const authors = JSON.parse(body)
+            console.log(authors)
+        }else{
+            console.log(response.statusCode , response.statusMessage)
+        }                 //! 404                  Not Found
+    })
+}
+
+
+
+//3 LISTAR A UN AUTOR POR SU ID 
+const getAuthor = (id)=>{
+    request.get(URI+id+'/',(error,response,body)=>{
+        //validamos nuestra petición 
+        if(response.statusCode === 200){
+            //Se convierte a un objeto de Javascript 
+            const author = JSON.parse(body)
+            console.log(author)
+        }else{
+            console.log('ID NO VALIDO', response.statusCode , response.statusMessage)
+        }  
+    })
+}
+
+
+//Crteamos un author en la API. Para ello cremos un JSON con la info del author 
+
+
+const createAuthor = (jsonData) =>{
+    const objectConfig = {
+        url:URI, //Endpoint para crear el autor 
+        form: jsonData //Esta es la data del autor al crear 
+    }
+
+    request.post(objectConfig,(error,response,body)=>{
+        if(response.statusCode === 201){
+            const createauthor = JSON.parse(body)
+            console.log("AUTOR CREADO EXITOSAMENTE: " + "\n", createauthor)
+        }else{
+            console.log("Autor NO creado exitosamente", response.statusCode , response.statusMessage)
+        }  
+    })
+
+}
+
+
+  module.exports = {
+    listAuthors,
+    getAuthor,
+    createAuthor,
+  } 
